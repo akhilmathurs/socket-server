f275da11c64e645c2f5b45884c63bd97ad1af1bc
Orchestra <akhilmathurs@gmail.com>
2022-11-24 08:45:37 +0000

    Updates

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+
+// The phone will emit a 'pair_watch' event. 
+// On receiving this event, the server will emit a 'pairing_request' event. 'msg' is Optional. Could be Empty or a string. 
+// The watch will listen for the 'pairing_request' event
+
+  socket.on('pair_watch', msg => {
+    io.emit('pairing_request', msg);
   });
+
+// The watch will emit a 'pairing_response' event with msg = {"choice": "approve"} or {"choice": "reject"} 
+// On receiving this event, the server will emit a 'watch_response' event with the same message
+// The phone will listen for 'watch_response', and depending on the message (Approve/Reject), will either add the watch to the UI or ignore it. 
+  socket.on('pairing_response', msg => {
+    io.emit('watch_response', msg);
+  });
+  
 });
 
 http.listen(port, () => {
